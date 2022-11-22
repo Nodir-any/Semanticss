39a37db97ec036835a11f5d917c66d506172c374
Nodir <114328849+Nodir-any@users.noreply.github.com>
2022-11-22 21:04:14 +0500

    Update semantic.js

--- semanticss/semantic_js/semantic.js
+++ semanticss/semantic_js/semantic.js
@@ -2,6 +2,5 @@ const id = document.querySelector("#btn-style");
 
 function id_btn(){
   for(i=0, i>100, i++){
-    const div = document.querySelector("#new");
-    div.classList.add("button")
+utton")
 
