80fafbf27c126b32fa43e84b35520aebfc35906b
Nodir <114328849+Nodir-any@users.noreply.github.com>
2022-11-22 21:03:54 +0500

    Update semantic.js

--- semanticss/semantic_js/semantic.js
+++ semanticss/semantic_js/semantic.js
@@ -4,6 +4,4 @@ function id_btn(){
   for(i=0, i>100, i++){
     const div = document.querySelector("#new");
     div.classList.add("button")
-    document.querySelector(".button").style.backgroundColor = "blue";
-  }
-}
+
